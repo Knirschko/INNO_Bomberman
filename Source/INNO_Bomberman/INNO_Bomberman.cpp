8fb41a25a7ea3032f2108438cb78269d30c4f564
Knirschko <nickb2345@gmail.com>
2022-10-25 10:01:45 +0200

    deleted old version
    für eine von Grund auf neue Version wird die alte entfernt

--- Source/INNO_Bomberman/INNO_Bomberman.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "INNO_Bomberman.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, INNO_Bomberman, "INNO_Bomberman" );
-
-DEFINE_LOG_CATEGORY(LogINNO_Bomberman)
- 
